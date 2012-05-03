0d3db0e3d98db720a8150924f3ee1543b12eaf7a
golddave <golddave@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-03 20:28:01 +0000

    git-svn-id: https://plugins.svn.wordpress.org/mlb-standings/trunk@539632 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- MLBStandings.php
+++ MLBStandings.php
@@ -16,12 +16,13 @@ Change Log
 */
 
 function ShowMLBStandings() {
+	$options = get_option('MLBStandings_options');
 	$filename = dirname(__FILE__)."/standings.xml";
 	
 	$date = date('h', time());
 	
-	if (($date !== (get_option('hour'))) || (!file_exists($filename))) {
-		update_option('hour', $date);
+	if (($date !== $options['hour']) || (!file_exists($filename))) {
+		update_option($options['hour'], $date);
 		
 		$sourcefile = "http://erikberg.com/mlb/standings.xml";
 		
@@ -34,8 +35,9 @@ function ShowMLBStandings() {
 	
 	$type = $xml->xpath("//standing/standing-metadata/sports-content-codes/sports-content-code/@code-type");
 	$key = $xml->xpath("//standing/standing-metadata/sports-content-codes/sports-content-code/@code-key");
+	
 	for ($i = 0; $i < 12; $i++) {
-		if (($type[$i]=="division") && ($key[$i]==get_option('division'))){
+		if (($type[$i]=="division") && ($key[$i]==str_replace("-",".",$options['division']))){
 			$x = (($i+1)/2)-1;
 			$division = $xml->xpath("/sports-content/standing");
 			?>
@@ -44,8 +46,7 @@ function ShowMLBStandings() {
 			<?php
 			echo "<table><tr><th align='left'>Team</th><th align='right'>W</th><th align='right'>L</th><th align='right'>Pct.</th><th align='right'>GB</th></tr>";
 			for ($j = 0; $j < count($division[$x]->team); $j++) {
-				//echo "<tr>";
-				if ($division[$x]->team[$j]->{'team-metadata'}->name->attributes()->last == "Mets") {
+				if ($division[$x]->team[$j]->{'team-metadata'}->name->attributes()->last == $options['team']) {
 					echo "<tr class='team'><td align='left'>".$division[$x]->team[$j]->{'team-metadata'}->name->attributes()->last."</td><td align='right'>".$division[$x]->team[$j]->{'team-stats'}->{'outcome-totals'}->attributes()->wins."</td><td align='right'>".$division[$x]->team[$j]->{'team-stats'}->{'outcome-totals'}->attributes()->losses."</td><td align='right'>".$division[$x]->team[$j]->{'team-stats'}->{'outcome-totals'}->attributes()->{'winning-percentage'}."</td>";
 				} else {
 					echo "<tr><td align='left'>".$division[$x]->team[$j]->{'team-metadata'}->name->attributes()->last."</td><td align='right'>".$division[$x]->team[$j]->{'team-stats'}->{'outcome-totals'}->attributes()->wins."</td><td align='right'>".$division[$x]->team[$j]->{'team-stats'}->{'outcome-totals'}->attributes()->losses."</td><td align='right'>".$division[$x]->team[$j]->{'team-stats'}->{'outcome-totals'}->attributes()->{'winning-percentage'}."</td>";
@@ -57,73 +58,213 @@ function ShowMLBStandings() {
 				}
 			}
 			echo "</tr></table>";
-			update_option('timestamp', $xml->{'sports-metadata'}->attributes()->{'date-time'});
-			$time_hour=substr(get_option('timestamp'),11,2);
-			$time_minute=substr(get_option('timestamp'),14,2);
-			$time_seconds=substr(get_option('timestamp'),17,2);
+			update_option($options['timestamp'], $xml->{'sports-metadata'}->attributes()->{'date-time'});
+			$time_hour=substr($options['timestamp'],11,2);
+			$time_minute=substr($options['timestamp'],14,2);
+			$time_seconds=substr($options['timestamp'],17,2);
 			$temptime=$time_hour.":".$time_minute.":".$time_seconds;
 			putenv("TZ=US/Pacific");
 			$time=date("g:i A T", mktime($time_hour,$time_minute,$time_seconds));			
-			//echo "<p style='font-size:70%;'>Last updated: ".substr(get_option('timestamp'),5,2)."/".substr(get_option('timestamp'),8,2)."/".substr(get_option('timestamp'),0,4)." - ".$time."</p></div>";
-			echo "<p class='date'>Last updated: ".substr(get_option('timestamp'),5,2)."/".substr(get_option('timestamp'),8,2)."/".substr(get_option('timestamp'),0,4)."</p></div>";
+			echo "<p class='date'>Last updated: ".substr($options['timestamp'],5,2)."/".substr($options['timestamp'],8,2)."/".substr($options['timestamp'],0,4)."</p></div>";
 		}
 	}
 }
 
+register_activation_hook(__FILE__, 'MLBStandings_add_defaults');
+add_action('admin_init', 'MLBStandings_init' );
 add_action('admin_menu', 'MLBStandings_add_options_page');
+add_filter('plugin_action_links', 'MLBStandings_plugin_action_links', 10, 2);
 
-function MLBStandings_add_options_page() {
-	// Add a new menu under Options:
-	add_options_page('MLB Standings', 'MLB Standings', 'manage_options', __FILE__, 'MLBStandings_options_page');
+
+// ------------------------------------------------------------------------------
+// CALLBACK FUNCTION FOR: register_activation_hook(__FILE__, 'MLBStandings_add_defaults')
+// ------------------------------------------------------------------------------
+// THIS FUNCTION RUNS WHEN THE PLUGIN IS ACTIVATED. IF THERE ARE NO THEME OPTIONS
+// CURRENTLY SET, OR THE USER HAS SELECTED THE CHECKBOX TO RESET OPTIONS TO THEIR
+// DEFAULTS THEN THE OPTIONS ARE SET/RESET.
+//
+// OTHERWISE, THE PLUGIN OPTIONS REMAIN UNCHANGED.
+// ------------------------------------------------------------------------------
+
+// Define default option settings
+function MLBStandings_add_defaults() {
+	$tmp = get_option('MLBStandings_options');
+    if(($tmp['chk_default_options_db']=='1')||(!is_array($tmp))) {
+		delete_option('MLBStandings_options'); // so we don't have to reset all the 'off' checkboxes too! (don't think this is needed but leave for now)
+		$arr = array(	"division" => "MLB-NL-E",
+						"team" => "Mets",
+						"timestamp"=>"2006-09-10T04:09:00-07:00",
+						"hour", date('h', time())		);
+		update_option('MLBStandings_options', $arr);
+	}
 }
 
-// Create the options page
-function MLBStandings_options_page() {
+// ------------------------------------------------------------------------------
+// CALLBACK FUNCTION FOR: add_action('admin_init', 'MLBStandings_init' )
+// ------------------------------------------------------------------------------
+// THIS FUNCTION RUNS WHEN THE 'admin_init' HOOK FIRES, AND REGISTERS YOUR PLUGIN
+// SETTING WITH THE WORDPRESS SETTINGS API. YOU WON'T BE ABLE TO USE THE SETTINGS
+// API UNTIL YOU DO.
+// ------------------------------------------------------------------------------
+
+// Init plugin options to white list our options
+function MLBStandings_init(){
+	register_setting( 'MLBStandings_plugin_options', 'MLBStandings_options' );
+}
+
+// ------------------------------------------------------------------------------
+// CALLBACK FUNCTION FOR: add_action('admin_menu', 'MLBStandings_add_options_page');
+// ------------------------------------------------------------------------------
+// THIS FUNCTION RUNS WHEN THE 'admin_menu' HOOK FIRES, AND ADDS A NEW OPTIONS
+// PAGE FOR YOUR PLUGIN TO THE SETTINGS MENU.
+// ------------------------------------------------------------------------------
+
+// Add menu page
+function MLBStandings_add_options_page() {
+	add_options_page('MLB Standings Options Page', 'MLB Standings', 'manage_options', __FILE__, 'MLBStandings_render_form');
+	
+}
+
+// ------------------------------------------------------------------------------
+// CALLBACK FUNCTION SPECIFIED IN: add_options_page()
+// ------------------------------------------------------------------------------
+// THIS FUNCTION IS SPECIFIED IN add_options_page() AS THE CALLBACK FUNCTION THAT
+// ACTUALLY RENDER THE PLUGIN OPTIONS FORM AS A SUB-MENU UNDER THE EXISTING
+// SETTINGS ADMIN MENU.
+// ------------------------------------------------------------------------------
+
+// Render the Plugin options form
+function MLBStandings_render_form() {
 	?>
-	<div>
+	<div class="wrap">
+		
+		<!-- Display Plugin Icon, Header, and Description -->
+		<div class="icon32" id="icon-options-general"><br></div>
 		<h2>MLB Standings Options</h2>
 		
-		<form action="options.php" method="post">
-			<?php settings_fields('MLBStandings_options'); ?>
-			<?php do_settings_sections('MLBStandings'); ?>
-			<input name="Submit" type="submit" value="<?php esc_attr_e('Save Changes'); ?>" />
+		<!-- Beginning of the Plugin Options Form -->
+		<form method="post" action="options.php">
+			<?php settings_fields('MLBStandings_plugin_options'); ?>
+			<?php $options = get_option('MLBStandings_options'); ?>
+
+			<!-- Table Structure Containing Form Controls -->
+			<!-- Each Plugin Option Defined on a New Table Row -->
+			<table class="form-table">
+				<tr>
+					<th scope="row">Division <?php echo ($options['division']); ?></th>
+					<td>
+						<select name='MLBStandings_options[division]' id='mydiv'>
+							<option value='MLB-AL-E' <?php selected('MLB-AL-E', $options['division']); ?>>AL East</option>
+							<option value='MLB-AL-C' <?php selected('MLB-AL-C', $options['division']); ?>>AL Central</option>
+							<option value='MLB-AL-W' <?php selected('MLB-AL-W', $options['division']); ?>>AL West</option>
+							<option value='MLB-NL-E' <?php selected('MLB-NL-E', $options['division']); ?>>NL East</option>
+							<option value='MLB-NL-C' <?php selected('MLB-NL-C', $options['division']); ?>>NL Central</option>
+							<option value='MLB-NL-W' <?php selected('MLB-NL-W', $options['division']); ?>>NL West</option>
+						</select>
+						<span style="color:#666666;margin-left:2px;">Select the division you'd like to display on your blog.</span>
+					</td>
+				</tr>
+
+				<tr>
+					<th scope="row">Team <?php echo ($options['team']); ?></th>
+					<td>
+						<select name='MLBStandings_options[team]' id="MLB-AL-E" class="teams">
+							<option value='Orioles' <?php selected('Orioles', $options['team']); ?>>Baltimore Orioles</option>
+							<option value='Red Sox' <?php selected('Red Sox', $options['team']); ?>>Boston Red Sox</option>
+							<option value='Yankees' <?php selected('Yankees', $options['team']); ?>>New York Yankees</option>
+							<option value='Rays' <?php selected('Rays', $options['team']); ?>>Tampa Bay Rays</option>
+							<option value='Blue Jays' <?php selected('Blue Jays', $options['team']); ?>>Toronto Blue Jays</option>
+						</select>
+						
+						<select name='MLBStandings_options[team]' id="MLB-AL-C" class="teams">
+							<option value ="White Sox" <?php selected('White Sox', $options['team']); ?>>Chicago White Sox</option>
+							<option value ="Indians" <?php selected('Indians', $options['team']); ?>>Cleveland Indians</option>
+							<option value ="Tigers" <?php selected('Tigers', $options['team']); ?>>Detroit Tigers</option>
+							<option value ="Royals" <?php selected('Royals', $options['team']); ?>>Kansas City Royals</option>
+							<option value ="Twins" <?php selected('Twins', $options['team']); ?>>Minnesota Twins</option>
+						</select>
+						
+						<select name='MLBStandings_options[team]' id="MLB-AL-W" class="teams">
+							<option value ="Angels" <?php selected('Angels', $options['team']); ?>>Los Angeles Angels</option>
+							<option value ="Athletics" <?php selected('Athletics', $options['team']); ?>>Oakland Athletics</option>
+							<option value ="Mariners" <?php selected('Mariners', $options['team']); ?>>Seattle Mariners</option>
+							<option value ="Rangers" <?php selected('Rangers', $options['team']); ?>>Texas Rangers</option>
+						</select>
+						
+						<select name='MLBStandings_options[team]' id="MLB-NL-E" class="teams">
+							<option value ="Braves" <?php selected('Braves', $options['team']); ?>>Atlanta Braves</option>
+							<option value ="Marlins" <?php selected('Marlins', $options['team']); ?>>Miami Marlins</option>
+							<option value ="Mets" <?php selected('Mets', $options['team']); ?>>New York Mets</option>
+							<option value ="Phillies" <?php selected('Phillies', $options['team']); ?>>Philadelphia Phillies</option>
+							<option value ="Nationals" <?php selected('Nationals', $options['team']); ?>>Washington Nationals</option>
+						</select>
+						
+						<select name='MLBStandings_options[team]' id="MLB-NL-C" class="teams">
+							<option value ="Cubs" <?php selected('Cubs', $options['team']); ?>>Chicago Cubs</option>
+							<option value ="Reds" <?php selected('Reds', $options['team']); ?>>Cincinnati Reds</option>
+							<option value ="Astros" <?php selected('Astros', $options['team']); ?>>Houston Astros</option>
+							<option value ="Brewers" <?php selected('Brewers', $options['team']); ?>>Milwaukee Brewers</option>
+							<option value ="Pirates" <?php selected('Pirates', $options['team']); ?>>Pittsburgh Pirates</option>
+							<option value ="Cardinals" <?php selected('Cardinals', $options['team']); ?>>St. Louis Cardinals</option>
+						</select>
+						
+						<select name='MLBStandings_options[team]' id="MLB-NL-W" class="teams">
+							<option value ="Diamondbacks" <?php selected('Diamondbacks', $options['team']); ?>>Arizona Diamondbacks</option>
+							<option value ="Rockies" <?php selected('Rockies', $options['team']); ?>>Colorado Rockies</option>
+							<option value ="Dodgers" <?php selected('Dodgers', $options['team']); ?>>Los Angeles Dodgers</option>
+							<option value ="Padres" <?php selected('Padres', $options['team']); ?>>San Diego Padres</option>
+							<option value ="Giants" <?php selected('Giants', $options['team']); ?>>San Francisco Giants</option>
+						</select>
+
+						<span style="color:#666666;margin-left:2px;">Select the team you'd like bolded in the standings.</span>
+					</td>
+				</tr>
+			</table>
+			<p class="submit">
+			<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+			</p>
 		</form>
+
 	</div>
-	<?php
+	
+	<script type='text/javascript'>
+		function teamchanger() {
+			jQuery(".teams").hide();
+			jQuery("#" + jQuery("#mydiv").val()).show();
+		}
+		jQuery(document).ready(function() {
+			teamchanger()
+			jQuery('#mydiv').change(function(){
+				teamchanger() 
+			});
+		});
+	</script>
+
+
+	<?php	
 }
 
-// add the admin settings and such
-add_action('admin_init', 'MLBStandings_admin_init');
+// Display a Settings link on the main Plugins page
+function MLBStandings_plugin_action_links( $links, $file ) {
 
-function MLBStandings_admin_init(){
-	register_setting( 'MLBStandings_options', 'division' );
-	//update_option('division', 'MLB.AL.E');
-	register_setting( 'MLBStandings_options', 'timestamp' );
-	update_option('timestamp', '2006-09-10T04:09:00-07:00');
-	register_setting( 'MLBStandings_options', 'hour' );
-	update_option('hour', (date('h', time())));
-	add_settings_section('MLBStandings_main', 'Division', 'division_section_text', 'MLBStandings');
-	add_settings_field('division_text_string', 'Select the division you\'d like to display on your blog:', 'MLBStandings_setting_string', 'MLBStandings', 'MLBStandings_main');
+	if ( $file == plugin_basename( __FILE__ ) ) {
+		$MLBStandings_links = '<a href="'.get_admin_url().'options-general.php?page=mlb-standings/MLBStandings.php">'.__('Settings').'</a>';
+		// make the 'Settings' link appear first
+		array_unshift( $links, $MLBStandings_links );
+	}
+
+	return $links;
 }
 
-function division_section_text() {
-}
 
-function MLBStandings_setting_string() {
-$options = get_option('MLBStandings_options');
 
-?>
-<td><select name="division">
-	<?php $selection = get_option('division'); ?>
-	<option value ="MLB.AL.E" <?php if ($selection=='MLB.AL.E') { echo 'selected'; } ?> >AL East</option>
-	<option value ="MLB.AL.C" <?php if ($selection=='MLB.AL.C') { echo 'selected'; } ?> >AL Central</option>
-	<option value ="MLB.AL.W" <?php if ($selection=='MLB.AL.W') { echo 'selected'; } ?> >AL West</option>
-	<option value ="MLB.NL.E" <?php if ($selection=='MLB.NL.E') { echo 'selected'; } ?> >NL East</option>
-	<option value ="MLB.NL.C" <?php if ($selection=='MLB.NL.C') { echo 'selected'; } ?> >NL Central</option>
-	<option value ="MLB.NL.W" <?php if ($selection=='MLB.NL.W') { echo 'selected'; } ?> >NL West</option>
-</select></td>
-<?php
-}
+
+
+
+
+
+
+
 
 function download2($sourcefile, $filename) {
 	// create a new curl resource
